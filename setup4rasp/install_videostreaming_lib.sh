e80fc9b3f3b05d1b3604179a1cc48486e18b7533
Si Young Jang <spencerjang90@gmail.com>
2018-07-17 19:51:09 +0900

    Update install_videostreaming_lib.sh

--- setup4rasp/install_videostreaming_lib.sh
+++ setup4rasp/install_videostreaming_lib.sh
@@ -1,8 +1,12 @@
 # install imutils
 sudo pip install imutils
+sudo pip3 install opencv-python
+# then... we don't need the previous installation???
 
 sudo su
 cd ../surveillance/misc
 cp pivideostream.py /usr/local/lib/python3.5/site-packages/imutils/video
 cp videostream.py /usr/local/lib/python3.5/site-packages/imutils/video
 exit
+
+
