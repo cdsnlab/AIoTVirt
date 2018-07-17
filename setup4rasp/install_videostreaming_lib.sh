22d38485ce938129baae8d1235e7e83ae943e0b2
Si Young Jang <spencerjang90@gmail.com>
2018-07-17 19:47:28 +0900

    Update install_videostreaming_lib.sh

--- setup4rasp/install_videostreaming_lib.sh
+++ setup4rasp/install_videostreaming_lib.sh
@@ -1,3 +1,8 @@
 # install imutils
 sudo pip install imutils
 
+sudo su
+cd ../surveillance/misc
+cp pivideostream.py /usr/local/lib/python3.5/site-packages/imutils/video
+cp videostream.py /usr/local/lib/python3.5/site-packages/imutils/video
+exit
