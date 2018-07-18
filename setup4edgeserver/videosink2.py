643e512e67d7a5c44f5ca8211c3b9112351cdd41
Si Young Jang <spencerjang90@gmail.com>
2018-07-18 14:57:43 +0900

    Update videosink2.py

--- setup4edgeserver/videosink2.py
+++ setup4edgeserver/videosink2.py
@@ -14,17 +14,17 @@ connection = server_socket.accept()[0].makefile('rb')
 try:
     # Run a viewer with an appropriate command line. Uncomment the mplayer
     # version if you would prefer to use mplayer instead of VLC
-    cmdline = ['vlc', '--demux', 'h264', '-']
-    #cmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
-    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+    # cmdline = ['vlc', '--demux', 'h264', '-']
+    # vcmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
+    # player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
     while True:
         # Repeatedly read 1k of data from the connection and write it to
         # the media player's stdin
         data = connection.read(1024)
         if not data:
             break
-        player.stdin.write(data)
+        # player.stdin.write(data)
 finally:
     connection.close()
     server_socket.close()
-    player.terminate()
+    # player.terminate()
