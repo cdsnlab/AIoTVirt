0dec55adcbb095065897ee076b0dbba230894f11
Si Young Jang <spencerjang90@gmail.com>
2018-07-18 14:48:37 +0900

    Update install_videostreaming_lib.sh

--- setup4rasp/install_videostreaming_lib.sh
+++ setup4rasp/install_videostreaming_lib.sh
@@ -5,8 +5,6 @@ sudo pip3 install python-prctl
 # then... we don't need the previous installation???
 
 # this is due to a change in imutils codes for camera thread naming. 
-sudo su
 cd ../surveillance/misc
-cp pivideostream.py /usr/local/lib/python3.5/dist-packages/imutils/video
-cp videostream.py /usr/local/lib/python3.5/dist-packages/imutils/video
-exit
+sudo cp pivideostream.py /usr/local/lib/python3.5/dist-packages/imutils/video
+sudo cp videostream.py /usr/local/lib/python3.5/dist-packages/imutils/video
