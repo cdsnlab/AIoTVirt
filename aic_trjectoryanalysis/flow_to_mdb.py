751fdac86aeca29d0b471c6affe8ddb8df599126
Spencer90 <spencerjang90@gmail.com>
2020-09-22 14:06:30 +0900

    each vehicle as  [UID, SECTION, CAMID, TRACE]

---
+++ aic_trjectoryanalysis/flow_to_mdb.py
@@ -0,0 +1,60 @@
+"""
+[DONE]
+ we first save the traces by uid, section, camid, trace for all files. trace eventually contains [start, end]
+"""
+
+import os, sys, time
+from tqdm import tqdm
+import pandas as pd
+from collections import defaultdict
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client['aic_mtmc']
+mdb = db['uid_traces']
+
+allfiles=[]
+
+def get_camid_section(filename):
+    camid, section = None, None
+    items = filename.split('/')
+    for item in items:
+        if item.startswith("c0"):
+            camid = item
+        if item.startswith("S"):
+            section = item
+    return camid, section
+
+#* iterate through folder to find all gt.txt files.
+for (path, dir, files) in os.walk ("/home/spencer1/samplevideo/AIC20_track3_MTMC/"):
+    for filename in files:
+        ext = os.path.splitext(filename)[-1]
+        if ext == ".txt" and "gt" in os.path.splitext(filename)[0]:
+            #print("%s/%s" % (path, filename))
+            allfiles.append(path+"/"+filename)
+
+count=0
+
+for file in tqdm(allfiles):
+    xplots = []
+    yplots = []
+    #print(count)
+    if os.stat(file).st_size != 0:
+        df = pd.read_csv(file, header=None, delimiter=',')
+        items = defaultdict(list)
+        camid, section = get_camid_section(file)
+        for index, row in df.iterrows(): #[frame, ID, left, top, width, height, 1, -1, -1, -1]
+            #* however many ids there are.            
+            items[row[1]].append(str(row[0]))
+
+            
+        #print(items[row[1]]["x"], items[row[1]]["y"])   
+        #print(len(items))
+        for i in (items):
+            #print(items[i][0], items[i][-1])
+            inputrow = {"uid": str(i), "section": str(section), "camid": str(camid), "trace": list(items[i])}
+            print(inputrow)
+            mdb.insert_one(inputrow)
+            count+=1
+        #print(items)
+
