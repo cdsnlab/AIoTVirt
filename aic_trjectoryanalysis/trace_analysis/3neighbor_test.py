b7d91a533ac6048588ef7ab07e2e0fc64da80824
Spencer90 <spencerjang90@gmail.com>
2020-10-07 20:06:10 +0900

    fnished iteration

--- aic_trjectoryanalysis/trace_analysis/3neighbor_test.py
+++ aic_trjectoryanalysis/trace_analysis/3neighbor_test.py
@@ -5,9 +5,6 @@ OUTPUT: average number of frames processed, average number of reid-ed objects (v
 This program finds the number of cameras which needs to be turned on to track a vehicle. GIVEN that all re-id is PERFECT.
 #?Each simulation ends when the vehicle is no long in the network. Should we give it additonal time (e.g. 100 frames = 10seconds) ???
 """
-#TODO 
-# 1) pick a target id to track.
-# 2) if found, find next cameras, next trackable candidates 
 
 import os, sys, time
 from tqdm import tqdm
@@ -37,13 +34,35 @@ def get_candidates(framenumber, af, ids): # 같은 framenumber에 있는 모든 
                 items.append((camid, section, len(allobjects), sfn.values.tolist()))
     #print(items)
     return items
-def checkifsame(status, items):
+
+def checkifsame(camid, items):
     for item in items:
-        #print(item, status)
-        if str(status) == str(item[0]):
-            return True
+        if str(camid) == str(item[0]):
+            if item[3]:
+                return True
     return False
     
+def searchnextcamcandidates(src):
+    stitems={}
+    srclookup={"src": str(src)}
+    doc=list(stdb.find(srclookup, {"_id":0, "src":1, "dst": 1, "temporal":1}))
+    for d in doc:
+        stitems[d['dst']] = (min(d['temporal']), max(d['temporal']))
+        #sum(d['temporal']) / len(d['temporal'])
+    #print(stitems)
+    return stitems
+    #what should this return?   
+    # next cameras, earliest possible start time.
+
+def compare_both(predicted_cam, all_things):
+    for i in predicted_cam:
+        for j in all_things:
+            #print(i,j)
+            if j[0] == i:
+                if j[3]:
+                    return i
+    return -1
+
 def get_maxiter(ids):
     dblookup = {"uid": str(ids)}
     doc=list(iddb.find(dblookup, {"_id":0, "camid":1, "trace":1}))
@@ -63,20 +82,18 @@ db = client['aic_mtmc']
 iddb = db['uid_traces']
 stdb = db['spatio_temporal']
 
-#* iterate through folder to find all gt.txt files. find maximum number of frames to look through
-#? keep it here for the time being: we don't want our system to iterate more than needed
 allfiles=[]
-MAXLINE=0
+all_cameras = []
 for (path, dir, files) in os.walk ("/home/spencer1/samplevideo/AIC20_track3_MTMC/"):
     for filename in files:
         ext = os.path.splitext(filename)[-1]
         if ext == ".txt" and "gt" in os.path.splitext(filename)[0]:
-            #print("%s/%s" % (path, filename))
             allfiles.append(path+"/"+filename)
-            num_lines = sum(1 for line in open(path+"/"+filename))
-            
-            if MAXLINE < num_lines:
-                MAXLINE = num_lines
+            #print("%s/%s" % (path, filename))
+            camid, section = get_camid_section(path+"/"+filename)
+            all_cameras.append(camid)
+print(all_cameras)
+
 
 #* iterate through DB to find maximum number of ids.
 MAXUID = 0
@@ -89,89 +106,80 @@ for trace in allfind:
 count=0
 print("MAXUID: {}".format(MAXUID))
 
-for ids in tqdm(range(1,MAXUID)): # for all ids
+for ids in tqdm(range(7,MAXUID)): # for all ids
     MAXITER = get_maxiter(ids)
     #print(doc['trace'])
     items = None
     cumulative_processed =0 # cumulative number of frames looked at. this include frames that may not contain target vehicle in the view.
     cumulative_reid = 0 # cumulative number of objects being reid'd
-    STATUS = None # (prev status, cur status, camera number) # to get if it has 
+    STATUS = ("NF", "NF", -1) # (prev status, cur status, camera number) # to get if it has 
+    possible_transitions ={}
+    pt_start ={}
+    pt_end = {}
     for framenumber in range(1, MAXITER): # run through all gt files to find it at that framenumber. 
-        print("[INFO] framenumber: {}, STATUS {}".format(framenumber, STATUS))
-        items = get_candidates(framenumber, allfiles, ids) # (camid, sector, number of vehicles in the view, location of the target vehicle in the view)
-        #TODO1: if found (beginning): count the number of objects being re-id in that camera. Keep status updated (tracking T/F, camera number)
-        #TODO2: if in transition: count the number of frames being processed 
+        print("[INFO] framenumber: {}, STATUS {}, MAXITER {}".format(framenumber, STATUS, MAXITER))
+        items = get_candidates(framenumber, allfiles, ids) # (camid, sector, number of vehicles in the view, location of the target vehicle in the view) #* all instance seen across all cameras
         tvcandidates = []
-        for item in items:
-            if item[3]:
-                tvcandidates.append(item)
-        if tvcandidates: # its TRK
-            print(tvcandidates) 
-            if STATUS == None: 
+
+        if STATUS[1] == "NF":
+            for item in items: #* instances of the same TV thats seen at "framenumber"
+                if item[3]:
+                    tvcandidates.append(item)
+            print(tvcandidates)
+            if tvcandidates: #* found TV in one of tvcandidates
                 STATUS = ("NF", "TRK", tvcandidates[0][0])
-            elif STATUS[1] == "TRK":
-                if checkifsame(STATUS[2], tvcandidates): #* its still tracking from the same camera
-                    continue
-                else: #* its being seen from a different camera. Lets switch to it
-                    STATUS = ("TRK", "TRK", tvcandidates[0][0])
-                    print("[INFO] changing camera to {}".format(tvcandidates[0][0]))
-            cumulative_processed+=1
-            cumulative_reid+=1
-        else: # its either NF/L/TRS
-            if STATUS==None: #* Never seen before
-                STATUS=("NF","NF", -1)
-                print("[INFO] Never seen before...")
-            elif STATUS[1]=="TRK": #Its been trking 
-                STATUS = ("TRK", "TRS", -1)
-                print("[INFO] IN Transition...")
+            else:
+                print("[INFO] Not seen yet")
 
-            #TODO need to add previous status in STATUS so that it knows that its been TRK not NF. 
-        #! check if there its the first occurance. IF there are more than one at the same period, pick one at random.
-        #! check if its being notfound(nf)/tracked(trk)/lost(l)/transition(trs)
-        #print (tvcandidates)
-        # if tvcandidates:
-        #     if STATUS == None:
-        #         STATUS = (True, )
-        '''
-        for item in items:
-            if item[3] : #* if there is TV
-                if STATUS == None: #* its the beginning
-                    STATUS = (True, item[0])
-                    cumulative_processed+=1
-                    cumulative_reid+=item[2]
+        elif STATUS[1] == "TRK":
+            for item in items: #* instances of the same TV thats seen at "framenumber"
+                if item[3]:
+                    tvcandidates.append(item)
+            print(tvcandidates)
+            if tvcandidates and checkifsame(STATUS[2], tvcandidates): #* TV is seen from the same camera
+                continue
+            elif tvcandidates and not checkifsame(STATUS[2], tvcandidates): #* TV is seen from a different camera
+                STATUS = ("TRK", "TRK", tvcandidates[0][0])
+                print("[INFO] changing camera to {}".format(tvcandidates[0][0]))
+            elif not tvcandidates: #* None of the cameras see TV
+                STATUS = ("TRK", "TRS", STATUS[2])
+                possible_transitions = searchnextcamcandidates(STATUS[2])
+                for key, value in possible_transitions.items():
+                    pt_start[key] = value[0] + framenumber
+                    pt_end[key] = value[1] + framenumber
+                print(pt_start, pt_end)
+        elif STATUS[1] == "TRS":
+            if not possible_transitions:
+                STATUS = ("TRS", "L", STATUS[2])
+                continue
+            for k, v in possible_transitions.items():
+                print(framenumber, pt_start[k] , pt_end[k])
+                if pt_start[k] <= framenumber and pt_end[k] > framenumber:
+                    existence = compare_both(k, items)
+                    if existence!=-1:
+                        STATUS = ("TRS", "TRK", existence)
+                        possible_transitions={}
+                elif pt_start[k]  > framenumber and pt_end[k] > framenumber:
+                    print("Lets wait")
+                elif pt_start[k] < framenumber and pt_end[k] < framenumber:
+                    possible_transitions.pop(k)
+                    pt_start.pop(k)
+                    pt_end.pop(k)
+                    print("poped: {}".format(k))
                     break
-                else: #* its seen TV before.
-                    # check if it is the same ID. #TODO add it to a temp list, and then see if its there.
-                    if item[1] == STATUS[1]: # if ID, continue tracking
-                        print("continue tracking")
-                        cumulative_processed+=1
-                        cumulative_reid+=item[2]
-                        break
-                    else: # if it isn't, its transfered to another. #! what if its not ordered???
-                        STATUS[1] = item[1]
+                
+            
+        elif STATUS[1] == "L":  
+            for cam in all_cameras: #* FIXMI FIRST do BF until you find it 
+                if checkifsame(cam, items):
+                    STATUS = ("NF", "TRK", cam)
+                    print("[INFO] Found at {}".format(cam))
+                    break
+        
+#!10/07 TODO
+# -> 이것부터 해보자. spatio-temporal 연관성
+# ---> 1) camera 단위로 + Sector 단위로 
+# SUM REID 개수 샐것. 
+# accuracy metric 넣을 것. 
 
-
-            else: # does not have TV
-                if STATUS == None: # keep searching
-                    continue
-                else: # its lost.
-                    STATUS[1] = -1
-        '''
-                    
-
-# for file in tqdm(allfiles):
-#     xplots = []
-#     yplots = []
-#     #print(count)
-#     if os.stat(file).st_size != 0:
-#         df = pd.read_csv(file, header=None, delimiter=',')
-#         items = defaultdict(lambda: defaultdict(list))
-#         camid, section = get_camid_section(file)
-#         for index, row in df.iterrows(): #[frame, ID, left, top, width, height, 1, -1, -1, -1]
-#             #* however many ids there are.            
-#             centx = row[2] + (row[4] / 2)
-#             centy = row[3] + (row[5] / 2)
-
-#             items[row[1]]["x"].append(centx) #id, x: list, y: list
-#             items[row[1]]["y"].append(centy)
-#             #print(row[1])
+# BF 구현                
