acf7afd7f68d82d515c25260f6924ed58c1a6e73
Tung Nguyen <tung@lugia>
2021-09-30 18:52:59 +0900

    process img

--- carlasim_v2/carla_client.py
+++ carlasim_v2/carla_client.py
@@ -147,3 +147,40 @@ class ClientSideBoundingBoxes(object):
         world_sensor_matrix = np.linalg.inv(sensor_world_matrix)
         sensor_cords = np.dot(world_sensor_matrix, cords)
         return sensor_cords
+
+    @staticmethod
+    def get_matrix(transform):
+        """
+        Creates matrix from carla transform.
+        """
+
+        rotation = transform.rotation
+        location = transform.location
+        c_y = np.cos(np.radians(rotation.yaw))
+        s_y = np.sin(np.radians(rotation.yaw))
+        c_r = np.cos(np.radians(rotation.roll))
+        s_r = np.sin(np.radians(rotation.roll))
+        c_p = np.cos(np.radians(rotation.pitch))
+        s_p = np.sin(np.radians(rotation.pitch))
+        matrix = np.matrix(np.identity(4))
+        matrix[0, 3] = location.x
+        matrix[1, 3] = location.y
+        matrix[2, 3] = location.z
+        matrix[0, 0] = c_p * c_y
+        matrix[0, 1] = c_y * s_p * s_r - s_y * c_r
+        matrix[0, 2] = -c_y * s_p * c_r - s_y * s_r
+        matrix[1, 0] = s_y * c_p
+        matrix[1, 1] = s_y * s_p * s_r + c_y * c_r
+        matrix[1, 2] = -s_y * s_p * c_r + c_y * s_r
+        matrix[2, 0] = s_p
+        matrix[2, 1] = -c_p * s_r
+        matrix[2, 2] = c_p * c_r
+        return matrix
+
+    # Handle video recording
+    @staticmethod
+    def process_img(image, VIEW_HEIGHT, VIEW_WIDTH):
+        i = np.array(image.raw_data)
+        i2 = i.reshape((VIEW_WIDTH, VIEW_HEIGHT, 4))
+        i3 = i2[:, :, :3]
+        return i3
