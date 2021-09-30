281dec28d2f29c20d99ea9cb2f8cabc7fca70106
Tung Nguyen <tung@lugia>
2021-09-30 19:26:18 +0900

    run scripts for crystal, careful not to change ports

---
+++ carlasim_v2/run_scripts/crystal_GPU_4/sim_4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd '/home/tung/carla/CARLA_Shipping_0.9.11-dirty/LinuxNoEditor/PythonAPI/carlasim'
+source carla-env/bin/activate
+python run_simulation.py -p 2006 --start=11 --end=11 --record_mode=2 --script_name=$(echo ${0##*/} | cut -d'.' -f1)
