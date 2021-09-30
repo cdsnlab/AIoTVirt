44a3ae895f65bdd5b15e9c7b23cebf80f21ee7e8
Tung Nguyen <tung@lugia>
2021-09-30 18:37:49 +0900

    camera setup

--- carlasim_v2/carla_server.py
+++ carlasim_v2/carla_server.py
@@ -44,4 +44,50 @@ class SynchronousServer(object):
     
         #self.image_map = {cam: set() for cam in range()}
 
+    def parse_cam_config(self, config_file_path):
+        print('[INFO] Parsing the configurations...')
+        self.config = configparser.ConfigParser()
+        self.config.read(config_file_path)
+        
+        self.cam_count = int(self.config['GENERAL']['Cameras'])
+
+        self.view_width = int(self.config['GENERAL']['Image_Size_X'])
+        self.view_height = int(self.config['GENERAL']['Image_Size_Y'])
+        self.view_fov = int(self.config['GENERAL']['FOV'])
+        self.fps = int(self.config['GENERAL']['FPS'])
+        self.fixed_delta = 1 / self.fps
+
+
+        self.rgb_camera_list = []
+        self.semseg_camera_list = []
+        print('[INFO] Configurations Parsed')
+
+    def setup_camera(self):
+        for cam_id in range(self.cam_count):
+            camera_config = self.config['CAMERA_' + str(cam_id)]
+            location = carla.Location(x=float(camera_config['PosX']), y=float(camera_config['PosY']), z=float(camera_config['PosZ']))
+            rotation = carla.Rotation(roll=float(camera_config['Roll']), pitch=float(camera_config['Pitch']), yaw=float(camera_config['Yaw']))
+            fov = float(camera_config['FOV'])
+
+            calibration = np.identity(3)
+            calibration[0, 2] = self.view_width / 2.0
+            calibration[1, 2] = self.view_height / 2.0
+            calibration[0, 0] = calibration[1, 1] = self.view_width / \
+                (2.0 * np.tan(fov * np.pi / 360.0))
+            
+            camera_rgb = self.world.spawn_actor(
+                self.get_camera_blueprint(fov, 'sensor.camera.rgb'),
+                carla.Transform(location, rotation)
+            )
+            camera_rgb.calibration = calibration
+            self.rgb_camera_list.append(('RGB_'+str(cam_id), camera_rgb))
+
+            camera_semseg = self.world.spawn_actor(
+                self.get_camera_blueprint(fov, 'sensor.camera.semantic_segmentation'),
+                carla.Transform(location, rotation)
+            )
+            camera_semseg.calibration = calibration
+            self.semseg_camera_list.append(('SS_' + str(cam_id), camera_semseg))
+            self.camera_ids.append(cam_id)
+
     
