c2a299ba382b3aecae6bce72cb389bf98952eed8
YoungRaeKimm <nahappy15@naver.com>
2023-12-04 22:39:52 +0900

    ddp

---
+++ restore_degradation/ddp_train.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=1,3 python -m torch.distributed.launch --nproc_per_node=2 \
+    train.py \
+    -meta_train True \
+    -ddp True \
+    -model_name metaweather \
+    -exp_name ddptest
