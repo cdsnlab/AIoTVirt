1e296fdc0d6f766dce5f4917c7762ac714087806
YoungRaeKimm <nahappy15@naver.com>
2023-12-04 22:59:29 +0900

    nafnet code arch utl

--- restore_degradation/models/NAFNet/arch_util.py
+++ restore_degradation/models/NAFNet/arch_util.py
@@ -14,14 +14,6 @@ from torch.nn.modules.batchnorm import _BatchNorm
 
 from basicsr.utils import get_root_logger
 
-# try:
-#     from basicsr.models.ops.dcn import (ModulatedDeformConvPack,
-#                                         modulated_deform_conv)
-# except ImportError:
-#     # print('Cannot import dcn. Ignore this warning if dcn is not used. '
-#     #       'Otherwise install BasicSR with compiling dcn.')
-#
-
 @torch.no_grad()
 def default_init_weights(module_list, scale=1, bias_fill=0, **kwargs):
     """Initialize network weights.
@@ -232,35 +224,6 @@ def pixel_unshuffle(x, scale):
     w = hw // scale
     x_view = x.view(b, c, h, scale, w, scale)
     return x_view.permute(0, 1, 3, 5, 2, 4).reshape(b, out_channel, h, w)
-
-
-# class DCNv2Pack(ModulatedDeformConvPack):
-#     """Modulated deformable conv for deformable alignment.
-#
-#     Different from the official DCNv2Pack, which generates offsets and masks
-#     from the preceding features, this DCNv2Pack takes another different
-#     features to generate offsets and masks.
-#
-#     Ref:
-#         Delving Deep into Deformable Alignment in Video Super-Resolution.
-#     """
-#
-#     def forward(self, x, feat):
-#         out = self.conv_offset(feat)
-#         o1, o2, mask = torch.chunk(out, 3, dim=1)
-#         offset = torch.cat((o1, o2), dim=1)
-#         mask = torch.sigmoid(mask)
-#
-#         offset_absmean = torch.mean(torch.abs(offset))
-#         if offset_absmean > 50:
-#             logger = get_root_logger()
-#             logger.warning(
-#                 f'Offset abs mean is {offset_absmean}, larger than 50.')
-#
-#         return modulated_deform_conv(x, offset, mask, self.weight, self.bias,
-#                                      self.stride, self.padding, self.dilation,
-#                                      self.groups, self.deformable_groups)
-
 
 class LayerNormFunction(torch.autograd.Function):
 
