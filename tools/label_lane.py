e5a08308d9743857ba34251de9011841f766f515
Younggeol Cho <rangewing@kaist.ac.kr>
2023-12-11 20:10:59 +0900

    key input

--- tools/label_lane.py
+++ tools/label_lane.py
@@ -20,12 +20,29 @@ def main(path: str, lane_file: str = None):
 
     frame = cv2.imread('tools/lane/ex.png')
 
+
+    clicked = []
+    
+    if lane_file is not None:
+        with open(lane_file, 'r') as f:
+            for line in f:
+                k, x, y = line.split()
+                clicked.append((k, (int(x), int(y))))
+
+    cv2.setMouseCallback('frame', on_mouse(clicked, state))
+
+    tmp_dict = dict()
     while True:
         img = frame.copy()
         cv2.imshow('frame', img)
-        ch = cv2.waitKey(1)
 
-    
+        ch = cv2.waitKey(1)
+        if ch == 27:
+            break
+        elif ch in [ord(c) for c in 'qweasd']:
+            state['key'] = chr(ch)
+
+
 
 
 if __name__ == '__main__':
