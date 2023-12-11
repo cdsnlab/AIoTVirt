0264659a22e6677ef76eede8fadaa2a566fe5805
Younggeol Cho <rangewing@kaist.ac.kr>
2023-12-11 20:01:48 +0900

    average ok

--- tools/background.py
+++ tools/background.py
@@ -26,3 +26,14 @@ def average(path):
         else:
             break
     
+    print(f'Accumulating {len(frames)} frames')
+    pv = 1 / len(frames)
+    for frame in frames:
+        cv2.accumulate(pv * frame.astype(np.float32), acc)
+        
+        filtered, _ = filter_lanes(frame, cv2.COLOR_GRAY2BGR)
+        cv2.accumulate(pv * filtered.astype(np.float32), acc2)
+
+    
+    show_acc = cv2.GaussianBlur(acc, (9, 9), 1.0).astype(np.uint8)
+    cv2.imshow(show_acc)
