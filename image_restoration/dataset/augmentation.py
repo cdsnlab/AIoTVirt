9f2fa95b3e8483d7e1d5b9eb5c1e02018fe4200c
PantheraXpoint <nhatquang.panthera0207@gmail.com>
2024-10-13 01:35:14 +0900

    feat: add rand_bbox function for CutMix

--- image_restoration/dataset/augmentation.py
+++ image_restoration/dataset/augmentation.py
@@ -31,3 +31,19 @@ def categorical_sample(p_range):
         return p_range
     else:
         return p_range[np.random.randint(len(p_range))]
+
+def rand_bbox(size, lam):
+    H, W = size
+    cut_rat = np.sqrt(lam)
+    cut_w = int(W * cut_rat)
+    cut_h = int(H * cut_rat)
+
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
+
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+    return bbx1, bby1, bbx2, bby2
