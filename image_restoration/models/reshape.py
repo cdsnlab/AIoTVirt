17678963bf4bce32c3ab378ac7034c628f55cd88
chengjinghan <chengjh_cqut@163.com>
2024-10-12 18:10:50 +0900

    feat:add parse_BTN

--- image_restoration/models/reshape.py
+++ image_restoration/models/reshape.py
@@ -29,3 +29,16 @@ from_3d_to_6d = get_reshaper('(B T) (N H W) C -> B T N C H W')
 
 from_4d_to_3d = get_reshaper('(B T N) C H W -> (B T) (N H W) C')
 from_3d_to_4d = get_reshaper('(B T) (N H W) C -> (B T N) C H W')
+
+
+def parse_BTN(x):
+    if isinstance(x, torch.Tensor):
+        B, T, N = x.size()[:3]
+    elif isinstance(x, (tuple, list)):
+        B, T, N = x[0].size()[:3]
+    elif isinstance(x, dict):
+        B, T, N = x[list(x.keys())[0]].size()[:3]
+    else:
+        raise ValueError(f'unsupported type: {type(x)}')
+
+    return B, T, N
