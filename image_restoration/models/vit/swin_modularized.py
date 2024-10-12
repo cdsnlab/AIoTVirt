8c587ad473dd57ca3c25e7b53a6ce29df49ce2c9
chengjinghan <chengjh_cqut@163.com>
2024-10-12 18:18:47 +0900

    feat:add LayerNormModularized class

--- image_restoration/models/vit/swin_modularized.py
+++ image_restoration/models/vit/swin_modularized.py
@@ -49,3 +49,53 @@ class MlpModularized(nn.Module):
         x = self.fc2(x, t_idx)
         x = self.drop(x)
         return x
+
+
+class LayerNormModularized(nn.LayerNorm):
+    def __init__(self, n_tasks=0, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.n_tasks = n_tasks
+        if self.n_tasks > 0:
+            assert self.elementwise_affine
+            self.bias = nn.Parameter(repeat(self.bias.data, '... -> T ...', T=n_tasks).contiguous())
+
+    def forward(self, input, t_idx=None):
+        if self.n_tasks > 0:
+            assert t_idx is not None
+            output = F.layer_norm(input, self.normalized_shape, self.weight, None, self.eps)
+            return output + self.bias[t_idx][:, None]
+        else:
+            return F.layer_norm(
+                input, self.normalized_shape, self.weight, self.bias, self.eps)
+
+def window_partition(x, window_size):
+    """
+    Args:
+        x: (B, H, W, C)
+        window_size (int): window size
+
+    Returns:
+        windows: (num_windows*B, window_size, window_size, C)
+    """
+    B, H, W, C = x.shape
+    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+    windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
+    return windows
+
+
+def window_reverse(windows, window_size, H, W):
+    """
+    Args:
+        windows: (num_windows*B, window_size, window_size, C)
+        window_size (int): Window size
+        H (int): Height of image
+        W (int): Width of image
+
+    Returns:
+        x: (B, H, W, C)
+    """
+    B = int(windows.shape[0] / (H * W / window_size / window_size))
+    x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
+    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
+    return x
