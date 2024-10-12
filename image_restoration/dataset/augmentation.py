896160ea58615489d00561b61cfcc3324c70feea
PantheraXpoint <nhatquang.panthera0207@gmail.com>
2024-10-13 01:52:23 +0900

    feat: implement horizontal flip augmentation with transformation tracking

--- image_restoration/dataset/augmentation.py
+++ image_restoration/dataset/augmentation.py
@@ -18,26 +18,28 @@ def linear_sample(p_range):
     else:
         return p_range[0] + random.random()*(p_range[1] - p_range[0])
     
-
+    
 def log_sample(p_range):
     if isinstance(p_range, float):
         return p_range
     else:
         return math.exp(math.log(p_range[0]) + random.random()*(math.log(p_range[1]) - math.log(p_range[0])))
     
-
+    
 def categorical_sample(p_range):
     if isinstance(p_range, (float, int)):
         return p_range
     else:
         return p_range[np.random.randint(len(p_range))]
-
+    
+    
 def rand_bbox(size, lam):
     H, W = size
     cut_rat = np.sqrt(1. - lam)
     cut_w = int(W * cut_rat)
     cut_h = int(H * cut_rat)
 
+    # uniform
     cx = np.random.randint(W)
     cy = np.random.randint(H)
 
@@ -47,3 +49,43 @@ def rand_bbox(size, lam):
     bby2 = np.clip(cy + cut_h // 2, 0, H)
 
     return bbx1, bby1, bbx2, bby2
+
+
+class Augmentation:
+    pass
+
+
+class RandomHorizontalFlip(Augmentation):
+    def __init__(self):
+        self.augmentation = lambda x: torch.flip(x, dims=[-1])
+        
+    def __str__(self):
+        return 'RandomHorizontalFlip Augmentation'
+        
+    def __call__(self, *arrays, get_augs=False):
+        if random.random() < 0.5:
+            if len(arrays) == 1:
+                if get_augs:
+                    return self.augmentation(arrays[0]), self.augmentation
+                else:
+                    return self.augmentation(arrays[0])
+            else:
+                arrays_flipped = []
+                for array in arrays:
+                    arrays_flipped.append(self.augmentation(array))
+                if get_augs:
+                    return arrays_flipped, self.augmentation
+                else:
+                    return arrays_flipped
+        else:
+            if len(arrays) == 1:
+                if get_augs:
+                    return arrays[0], lambda x: x
+                else:
+                    return arrays[0]
+            else:
+                if get_augs:
+                    return arrays, lambda x: x
+                else:
+                    return arrays
+    
