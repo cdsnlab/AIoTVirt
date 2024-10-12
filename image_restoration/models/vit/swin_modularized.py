7b390e0c9849484ee2cd0b069d203b4988cbdcc9
chengjinghan <chengjh_cqut@163.com>
2024-10-12 18:26:56 +0900

    fix:SwinTransformerModularized

--- image_restoration/models/vit/swin_modularized.py
+++ image_restoration/models/vit/swin_modularized.py
@@ -197,6 +197,8 @@ class WindowAttention(nn.Module):
         # x = self.proj(x)
         flops += N * self.dim * self.dim
         return flops
+
+
 class SwinTransformerBlockModularized(nn.Module):
     r""" Swin Transformer Block.
 
@@ -326,6 +328,8 @@ class SwinTransformerBlockModularized(nn.Module):
         flops += self.dim * H * W
         return flops
 
+
+
 class PatchMerging(nn.Module):
     r""" Patch Merging Layer.
 
@@ -373,6 +377,8 @@ class PatchMerging(nn.Module):
         flops = H * W * self.dim
         flops += (H // 2) * (W // 2) * 4 * self.dim * 2 * self.dim
         return flops
+
+
 class BasicLayerModularized(nn.Module):
     """ A basic Swin Transformer layer for one stage.
 
@@ -441,6 +447,8 @@ class BasicLayerModularized(nn.Module):
         if self.downsample is not None:
             flops += self.downsample.flops()
         return flops
+
+
 class PatchEmbed(nn.Module):
     r""" Image to Patch Embedding
 
@@ -487,52 +495,7 @@ class PatchEmbed(nn.Module):
         if self.norm is not None:
             flops += Ho * Wo * self.embed_dim
         return flops
-class PatchEmbed(nn.Module):
-    r""" Image to Patch Embedding
 
-    Args:
-        img_size (int): Image size.  Default: 224.
-        patch_size (int): Patch token size. Default: 4.
-        in_chans (int): Number of input image channels. Default: 3.
-        embed_dim (int): Number of linear projection output channels. Default: 96.
-        norm_layer (nn.Module, optional): Normalization layer. Default: None
-    """
-
-    def __init__(self, img_size=224, patch_size=4, in_chans=3, embed_dim=96, norm_layer=None):
-        super().__init__()
-        img_size = to_2tuple(img_size)
-        patch_size = to_2tuple(patch_size)
-        patches_resolution = [img_size[0] // patch_size[0], img_size[1] // patch_size[1]]
-        self.img_size = img_size
-        self.patch_size = patch_size
-        self.patches_resolution = patches_resolution
-        self.num_patches = patches_resolution[0] * patches_resolution[1]
-
-        self.in_chans = in_chans
-        self.embed_dim = embed_dim
-
-        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
-        if norm_layer is not None:
-            self.norm = norm_layer(embed_dim)
-        else:
-            self.norm = None
-
-    def forward(self, x):
-        B, C, H, W = x.shape
-        # FIXME look at relaxing size constraints
-        assert H == self.img_size[0] and W == self.img_size[1], \
-            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
-        x = self.proj(x).flatten(2).transpose(1, 2)  # B Ph*Pw C
-        if self.norm is not None:
-            x = self.norm(x)
-        return x
-
-    def flops(self):
-        Ho, Wo = self.patches_resolution
-        flops = Ho * Wo * self.embed_dim * self.in_chans * (self.patch_size[0] * self.patch_size[1])
-        if self.norm is not None:
-            flops += Ho * Wo * self.embed_dim
-        return flops
 
 class SwinTransformerModularized(nn.Module):
     r""" Swin Transformer
