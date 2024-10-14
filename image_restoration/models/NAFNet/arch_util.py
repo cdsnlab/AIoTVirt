9b813d626c0851631385ac74461d3a863cea29db
wyhallenwu <wyhforwork66@gmail.com>
2024-10-14 20:29:35 +0900

    layernorm2d

--- image_restoration/models/NAFNet/arch_util.py
+++ image_restoration/models/NAFNet/arch_util.py
@@ -273,3 +273,14 @@ class LayerNormFunction(torch.autograd.Function):
         gx = 1. / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
         return gx, (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0), grad_output.sum(dim=3).sum(dim=2).sum(
             dim=0), None
+
+class LayerNorm2d(nn.Module):
+
+    def __init__(self, channels, eps=1e-6):
+        super(LayerNorm2d, self).__init__()
+        self.register_parameter('weight', nn.Parameter(torch.ones(channels)))
+        self.register_parameter('bias', nn.Parameter(torch.zeros(channels)))
+        self.eps = eps
+
+    def forward(self, x):
+        return LayerNormFunction.apply(x, self.weight, self.bias, self.eps)
