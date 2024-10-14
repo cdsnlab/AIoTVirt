87bf120dcd8d0a5e4eae5d3f3e82673c87c492ad
wyhallenwu <wyhforwork66@gmail.com>
2024-10-14 20:35:29 +0900

    IB check image size

--- image_restoration/models/NAFNet/NAFNet_arch.py
+++ image_restoration/models/NAFNet/NAFNet_arch.py
@@ -283,3 +283,10 @@ class NAFNet_IB(nn.Module):
         x2 = x2 + inp
 
         return x2[:, :, :H, :W], x1[:, :, :H, :W] # restored, degradation
+    
+    def check_image_size(self, x):
+        _, _, h, w = x.size()
+        mod_pad_h = (self.padder_size - h % self.padder_size) % self.padder_size
+        mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
+        x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
+        return x
