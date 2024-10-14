65309f8055f65cd04f9dcfafa3deb8d3b7bfe1ee
wyhallenwu <wyhforwork66@gmail.com>
2024-10-14 20:29:16 +0900

    layernorm

--- image_restoration/models/NAFNet/arch_util.py
+++ image_restoration/models/NAFNet/arch_util.py
@@ -246,3 +246,30 @@ def pixel_unshuffle(x, scale):
 #         return modulated_deform_conv(x, offset, mask, self.weight, self.bias,
 #                                      self.stride, self.padding, self.dilation,
 #                                      self.groups, self.deformable_groups)
+
+class LayerNormFunction(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x, weight, bias, eps):
+        ctx.eps = eps
+        N, C, H, W = x.size()
+        mu = x.mean(1, keepdim=True)
+        var = (x - mu).pow(2).mean(1, keepdim=True)
+        y = (x - mu) / (var + eps).sqrt()
+        ctx.save_for_backward(y, var, weight)
+        y = weight.view(1, C, 1, 1) * y + bias.view(1, C, 1, 1)
+        return y
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        eps = ctx.eps
+
+        N, C, H, W = grad_output.size()
+        y, var, weight = ctx.saved_variables
+        g = grad_output * weight.view(1, C, 1, 1)
+        mean_g = g.mean(dim=1, keepdim=True)
+
+        mean_gy = (g * y).mean(dim=1, keepdim=True)
+        gx = 1. / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
+        return gx, (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0), grad_output.sum(dim=3).sum(dim=2).sum(
+            dim=0), None
