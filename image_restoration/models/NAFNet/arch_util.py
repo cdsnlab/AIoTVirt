54db9d92dbffc8c21cb64c4037ad6b70d6ecc952
wyhallenwu <wyhforwork66@gmail.com>
2024-10-14 20:26:44 +0900

    make layer

--- image_restoration/models/NAFNet/arch_util.py
+++ image_restoration/models/NAFNet/arch_util.py
@@ -44,3 +44,18 @@ def default_init_weights(module_list, scale=1, bias_fill=0, **kwargs):
                 init.constant_(m.weight, 1)
                 if m.bias is not None:
                     m.bias.data.fill_(bias_fill)
+
+def make_layer(basic_block, num_basic_block, **kwarg):
+    """Make layers by stacking the same blocks.
+
+    Args:
+        basic_block (nn.module): nn.module class for basic block.
+        num_basic_block (int): number of blocks.
+
+    Returns:
+        nn.Sequential: Stacked blocks in nn.Sequential.
+    """
+    layers = []
+    for _ in range(num_basic_block):
+        layers.append(basic_block(**kwarg))
+    return nn.Sequential(*layers)
