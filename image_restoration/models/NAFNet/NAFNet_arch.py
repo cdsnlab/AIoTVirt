c4a16784a569a3c568066c2ebac974454a1d304e
wyhallenwu <wyhforwork66@gmail.com>
2024-10-14 20:36:56 +0900

    NAFNET local

--- image_restoration/models/NAFNet/NAFNet_arch.py
+++ image_restoration/models/NAFNet/NAFNet_arch.py
@@ -410,3 +410,15 @@ class NAFNet_IB_2(nn.Module):
         mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
         x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
         return x
+
+class NAFNetLocal(Local_Base, NAFNet):
+    def __init__(self, *args, train_size=(1, 3, 256, 256), fast_imp=False, **kwargs):
+        Local_Base.__init__(self)
+        NAFNet.__init__(self, *args, **kwargs)
+
+        N, C, H, W = train_size
+        base_size = (int(H * 1.5), int(W * 1.5))
+
+        self.eval()
+        with torch.no_grad():
+            self.convert(base_size=base_size, train_size=train_size, fast_imp=fast_imp)
