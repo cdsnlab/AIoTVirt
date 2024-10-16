256bd7fa866d9a59f4c652607fbf28e4760355e9
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-10-16 16:32:12 +0900

    feat: init training

--- image_restoration/train.py
+++ image_restoration/train.py
@@ -18,3 +18,40 @@ from dataset.utils import TASK_DATASETS_TRAIN, TASK_DATASETS_TEST
 from dataset.factory import get_train_dataloader, get_support_data, get_val_dataloaders, get_eval_dataloader, get_finetune_dataloader, generate_support_data
 from models.model_factory import get_model
 
+warnings.filterwarnings(action='ignore')
+plt.switch_backend('agg')
+
+# --- Parse hyper-parameters  --- #
+parser = argparse.ArgumentParser(description='Hyper-parameters for network')
+parser.add_argument('--meta_train', default=True, type=str2bool)
+parser.add_argument('--ddp', default=False, type=str2bool)
+parser.add_argument('--stage', default=0, type=int, help='0:pretrain, 1:adaptation, 2:test')
+parser.add_argument('--case', default=0, type=int, help='0-6')
+parser.add_argument('--model_name', default='metaweather', type=str)
+parser.add_argument('--exp_name', help='directory for saving the networks of the experiment', type=str)
+parser.add_argument('--checkpoint', help='path to checkpoint', type=str, default=None)
+
+# --- DDP --- #
+parser.add_argument('--local_rank', type=int)
+parser.add_argument('--world_size', type=int)
+
+args = parser.parse_args()
+
+config = get_config(args, meta=args.meta_train)
+
+# --- Distributed Data Parallel initialize --- #
+if config.ddp:
+    raise NotImplementedError('DDP is not supported yet.')
+
+# --- Set Seed --- #
+seed = 19
+if seed is not None:
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    random.seed(seed)
+    print('Seed:\t{}'.format(seed))
+
+print('--- Hyper-parameters for training ---')
+print('learning_rate: {}\ncrop_size: {}\ntrain_batch_size: {}\nval_batch_size: {}\n'.format(config.lr, config.img_size,
+        config.batch_size, config.val_batch_size))
