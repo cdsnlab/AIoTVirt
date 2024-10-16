5797bc3e91b8cfb63fff9b6476d76c731fee3dd4
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-10-16 16:21:46 +0900

    feat: finish config parsing

--- image_restoration/config.py
+++ image_restoration/config.py
@@ -25,3 +25,17 @@ def get_config(args: argparse.Namespace, meta=True) -> EasyDict:
     for key in args.__dir__():
         if key[:2] != '__' and getattr(args, key) is not None:
             setattr(config, key, getattr(args, key))
+
+    with open('configs/data.yaml') as f:
+        data_conf = yaml.safe_load(f)
+        config.data_root = data_conf['data_root']
+        config.datasets = data_conf['datasets']
+
+    if 'task' not in config:
+        from dataset.utils import TASK_DATASETS_TEST, TASK_DATASETS_TRAIN
+        config.task = TASK_DATASETS_TEST[config.case] if config.stage > 1 else TASK_DATASETS_TRAIN
+
+    if 'checkpoint' not in config:
+        config.checkpoint = None
+
+    return config
